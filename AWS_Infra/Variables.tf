f96a76c0807a16caec30311825111c6840480539
0x1Luffy <chetankesare06@gmail.com>
2025-07-02 13:11:37 +0530

    WIP: Resuable Variables for Multi-Env

---
+++ AWS_Infra/Variables.tf
@@ -0,0 +1,14 @@
+variable "env_name" {
+  description = "Reuseable infra Variables"
+  type = string
+}
+
+variable "ami_id" {
+  description = "Reuseable AMI ID"
+  type = string
+}
+
+variable "ec2_instance" {
+  description = "Reuseable EC2 Instance description"
+  type = string
+}
