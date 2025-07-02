407630f07425bb977e037a8eeab5c69802d05bef
0x1Luffy <chetankesare06@gmail.com>
2025-07-02 13:25:53 +0530

    Multi-ENV EC2 Instance

---
+++ AWS_Infra/EC2_Instance.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "my-ec2" {
+   ami = var.ami_id
+   instance_type = var.ec2_instance
+   key_name = aws_key_pair.my-pub-key.key_name # Change accorindly
+   security_groups = [aws_security_group.My_security_group.name] # change accordingly
+   tags = {
+     name = "${var.env_name}-EC2 Instance"
+   }
+ }
